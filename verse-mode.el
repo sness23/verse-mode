3586195f3c42767005bfbd4131c5fa9de0ca13be
sness <snessnet@gmail.com>
2025-08-31 19:13:11 -0700

    verse-mode: add SPDX-License-Identifier and GPL boilerplate

--- verse-mode.el
+++ verse-mode.el
@@ -5,7 +5,20 @@
 ;; Package-Requires: ((emacs "26.1"))
 ;; Keywords: languages, tools
 ;; URL: https://github.com/sness23/verse-mode
-;; License: GPL-3.0-or-later
+;; SPDX-License-Identifier: GPL-3.0-or-later
+;;
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
